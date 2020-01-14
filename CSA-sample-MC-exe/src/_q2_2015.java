ca5ffa59d80b333abc2a47680c6a7a937ca801ae
1845930 <1845930@20.8.20.247>
2020-01-14 13:48:28 -0800

    a

---
+++ CSA-sample-MC-exe/src/_q2_2015.java
@@ -0,0 +1,24 @@
+
+public class _q2_2015 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	for (int r = 3; r>0; r--)
+	{
+		int c;
+		
+		for (c = 1; c<r; c++)
+		{
+			System.out.print("-");
+		}
+		for (c = r; c<=3; c++);
+		{
+			System.
+			
+			out.print("*");
+		}
+	}
+	System.out.println();
+	}
+}
---
+++ CSA-sample-MC-exe/src/_q3_2015.java
@@ -0,0 +1,9 @@
+
+public class _q3_2015 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
