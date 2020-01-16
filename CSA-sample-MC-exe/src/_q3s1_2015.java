c4e21e3a53c1a6f4a3a38411b425cfa861f3b6a6
1845930 <1845930@20.8.20.247>
2020-01-16 12:38:28 -0800

    as

---
+++ CSA-sample-MC-exe/src/_q3s1_2015.java
@@ -0,0 +1,17 @@
+
+public class _q3s1_2015 {
+
+	public static void main(String[] args) {
+	for (int outer = 1; outer <= 6; outer++)
+		{
+		for (int inner = outer; inner <=6; inner++)
+			{
+			if (inner%2==0)
+				{
+					System.out.print(inner+"");
+				}
+			}
+		System.out.println();
+		}
+	}
+}	
---
+++ CSA-sample-MC-exe/src/general/Main.java
@@ -0,0 +1,44 @@
+package general;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+public class Main {
+	
+	//ArrayList of file content
+	public static ArrayList<String> ar = new ArrayList<String>();
+
+	
+	public static void main(String[] args)
+	{ 	
+		try
+		{
+			File file=new File("/Users/1901209/Desktop/q1.java");    //creates a new file instance  
+			FileReader fr=new FileReader(file);   //reads the file  
+			
+			BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream 
+			
+			String line; 
+			
+			while((line=br.readLine())!=null)  
+			{  
+				ar.add(line);      //appends line to string buffer
+			} 
+			
+			//CREATE a method to check make sure that there is no error.
+			for(int i = 0; i < ar.size(); i++)
+			{
+				System.out.println(ar.get(i));
+			}
+			
+			fr.close();    //closes the stream and release the resources
+		}
+		catch(IOException e)  
+		{  
+			e.printStackTrace();  
+		}  
+		
+	}
+}
