57cd403ab4a1cadff3647b800fa1b4281a85bded
1900048 <1900048@20.8.20.87>
2020-01-27 13:01:45 -0800

    add shuff

---
+++ CSA-sample-MC-exe/src/_02using_objects/q_2.java
@@ -0,0 +1,32 @@
+package _02using_objects;
+
+public class q_2 
+{
+	/*
+	 * Consider the following declarations.
+	 *  */
+	
+	  public interface Comparable
+	  {
+	    int compareTo(Object other); 
+	  
+	  }
+	  
+	 public class SomeClass implements Comparable 
+	 {
+	    // ... other methods not shown 
+	 }
+	 /*
+	 * 	Which of the following method signatures of compareTo will satisfy the Comparable interface requirement?
+	 * III. public int compareTo(Object other)
+	 * III. public int compareTo(SomeClass other)
+	 * III. public boolean compareTo(Object other)
+	 * 
+	 * (a) I only
+	 * (b) II only
+	 * (c) III only
+	 * (d) I and II only 
+	 * (e) I, II, and III
+	 */
+	
+}
